6f9b8c1ecfcdf544fb56bdee349e3b3e39ce5959
Guilherme da Silva Benevides <gsbenevides2@gmail.com>
2023-07-18 13:59:43 -0300

    fix:cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,7 +5,9 @@ const routes = require('./routes');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin:'*'
+}));
 app.use(express.json());
 app.use(routes);
 app.use(errors());
